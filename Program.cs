e212148018fe6634abaf3c1c6f8e265087648da9
WELLINGTON MARTEVI DE LIMA <t-wellington.m.lima@bradesco.com.br>
2022-04-13 22:09:54 -0300

    basic cshap

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace c_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string[] names = { "Maria","João"};
+           Console.WriteLine($"Nomes: {names[0]} e {names[1]}!");
+
+           for (int i = 0; i < 2; i++)
+           {
+               Console.WriteLine(names[i]);
+               
+           }
+        }
+    }
+}
